a1a6c97a213475d6191a8c70273d8330f6ec09ad
QuangSang <quangsang222@gmail.com>
2018-03-08 21:35:22 +0700

    a

--- modules/child/portfolio.php
+++ modules/child/portfolio.php
@@ -3,4 +3,21 @@
     <h2 class="title">portfolio</h2>
     <span>my best work</span>
   </div>
+  <div class="container filter">
+    <div class="row">
+      <div class="col-md-12 text-center filter--nav">
+        <nav>
+          <span class="active">All</span>
+          <span>Website</span>
+          <span>Banner</span>
+          <span>Emailing</span>
+        </nav>
+      </div>
+    </div>
+    <div class="row filter--content">
+      <div class="col-md-3 filter--content__item">
+        <span>test</span>
+      </div>
+    </div>
+  </div>
 </section>
